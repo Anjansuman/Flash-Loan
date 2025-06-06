97481cde5addcbce2028c3c318db04c15f54b764
Anjan Suman <anjansuman80@gmail.com>
2025-06-06 23:22:51 +0530

    flash loan contract complete.

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Router01.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "./libraries/SafeERC20.sol";
@@ -18,7 +19,7 @@ contract FlashLoan {
     address private constant PANCAKE_ROUTER =
         0x10ED43C718714eb63d5aA57B78B54704E256024E;
 
-    // token addresses in BSC, these will be used to flash loan, the original one contains all the tokens
+    // token addresses in BSC, these will be used to flash loan, the original swap contains all the tokens
     address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
     address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
     address private constant CROX = 0x2c094F5A7D1146BB93850f629501eB749f6Ed491;
@@ -66,20 +67,65 @@ contract FlashLoan {
         address token0 = IUniswapV2Pair(msg.sender).token0();
         address token1 = IUniswapV2Pair(msg.sender).token1();
 
+        // again fetching the pool address
         address gotPair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(token0, token1);
 
+        // checking if both of them are equal
         requrie(msg.sender == gotPair, "both pairs didn't match");
 
-        require(_sender == address(this), "Sender must be this address");
+        // the sender must be this contract
+        require(_sender == address(this), "Sender must be this contract address");
 
         // this is to decode the data back to variables
         (address _tokenBorrow, uint256 _amount, address userAddress) = abi.decode(_data, (address, uint256, address));
 
+        // fee calculation
         uint256 fee = ((amount * 3) / 997) + 1; // this fee is calculated as per pancake swap flash loans, can be changed as per other orgs
         uint256 repayAmount = _amount + fee;
 
         uint loanAmount = _amount0 > 0 ? _amount0 : _amount1;
-        
 
+        // Placing Triangular Arbitrage [I am assuming the user chose BUSD for it's loan, so I'm hardcoding others coins for trade]
+        uint trade1Coin = placeTrade(BUSD, CROX, loanAmount);
+        uint trade2Coin = placeTrade(CROX, CAKE, trade1Coin);
+        uint trade3Coin = placeTrade(CAKE, BUSD, trade2Coin);
+
+        // checking profit or loss
+        bool result = checkResult(repayAmount, trade3Coin);
+
+        require(result, "Loss occured, reverting!");
+
+        // passed the required statement, that means profit occured
+
+        // transfering profit amount to user
+        IERC20(BUSD).transfer(userAddress, trade3Coin - repayAmount);
+        IERC20(_tokenBorrow).transfer(gotPair, repayAmount);
+
+    }
+
+    function placeTrade(address _fromToken, address _toToken, uint amountIn) private returns(uint) {
+        address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(_fromToken, _toToken);
+        
+        require(pair != address(0), "Pool does not exist");
+
+        address[] memory path = new address[](2); // array of size 2
+
+        path[0] = _fromToken;
+        path[1] = _toToken;
+
+        uint amountRequired = IUniswapV2Router01(PANCAKE_FACTORY).getAmountsOut(amountIn, path[1]);
+
+        uint amountReceived = IUniswapV2Router01(PANCAKE_ROUTER).swapExactTokensForTokens(amountIn, amountRequired, path, address(this), deadline)[1];
+
+        require(amountReceived > 0, "Transaction aborted!");
+        return amountReceived;
+    }
+
+    function checkResult(uint repayAmount, uint tradeAmount) private returns(bool) {
+        return repayAmount > tradeAmount;
+    }
+
+    function getBalanceOfToken(address _address) public returns(uint) {
+        reutrn IERC20(_address).balanceOf(address(this));
     }
 }
